6170819159cd87ae5d7e5611bae9ae9975ab040f
JackBekket <jackatwork@yandex.ru>
2017-02-18 17:06:39 +0300

    truffle.js

--- truffle.js
+++ truffle.js
@@ -1,9 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// 12-word mnemonic
+var mnemonic = "rift saddling evade tutor escapade helper emboss safely scheme refusal flashback deeply";
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+      network_id: 3 // official id of the ropsten network
     }
   }
 };
