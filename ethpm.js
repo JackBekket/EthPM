f9bd0444d705703f6e28f0834862cba2a8a04162
JackBekket <jackatwork@yandex.ru>
2017-02-18 17:12:13 +0300

    test ethpm config

---
+++ ethpm.js
@@ -0,0 +1,16 @@
+{
+  "package_name": "escrow-single",
+  "version": "0.0.1",
+  "description": "Simple contract of single escrow",
+  "authors": [
+    "SergeyPonomarev <jackatwork@yandex.ru>"
+  ],
+  "keywords": [
+    "ethereum",
+    "addition"
+  ],
+  "dependencies": {
+    "owned": "^0.0.1"
+  },
+//  "license": "MIT"
+}
